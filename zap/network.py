93aabf390016b52d8b07d2be5a479ba191b8b20e
degleris1 <degleris1@gmail.com>
2024-03-18 12:09:10 -0700

    indexable dispatch outcome with shape and block indices

--- zap/network.py
+++ zap/network.py
@@ -1,35 +1,114 @@
 import itertools
 import cvxpy as cp
 import numpy as np
+import scipy.sparse as sps
 
 from dataclasses import dataclass
-from collections import namedtuple
 from itertools import repeat
+from functools import cached_property
+from collections.abc import Sequence
 
 from zap.devices.abstract import AbstractDevice
 from zap.devices.ground import Ground
 
-DispatchOutcomeBase = namedtuple(
-    "DispatchOutcome",
-    [
-        "global_angle",
-        "power",
-        "angle",
-        "local_variables",
-        "prices",
-        "phase_duals",
-        "local_equality_duals",
-        "local_inequality_duals",
-        "problem",
-        "ground",
-    ],
-)
 
+@dataclass
+class DispatchOutcome(Sequence):
+    phase_duals: object
+    local_equality_duals: object
+    local_inequality_duals: object
+    local_variables: object
+    power: object
+    angle: object
+    prices: object
+    global_angle: object
+    problem: object = None
+    ground: object = None
 
-class DispatchOutcome(DispatchOutcomeBase):
+    def __getitem__(self, i):
+        match i:
+            case 0:
+                return self.phase_duals
+            case 1:
+                return self.local_equality_duals
+            case 2:
+                return self.local_inequality_duals
+            case 3:
+                return self.local_variables
+            case 4:
+                return self.power
+            case 5:
+                return self.angle
+            case 6:
+                return self.prices
+            case 7:
+                return self.global_angle
+            case _:
+                raise IndexError
+
+    def __len__(self):
+        return 8
+
     @property
     def time_horizon(self):
         return self.global_angle.shape[1]
+
+    @cached_property
+    def size(self):
+        return self.vectorize().size
+
+    @cached_property
+    def shape(self):
+        mu_shape = [np.shape(mu) for mu in self.phase_duals]
+        lambda_eq_shape = [[np.shape(lam_k) for lam_k in lam] for lam in self.local_equality_duals]
+        lambda_ineq_shape = [
+            [np.shape(lam_k) for lam_k in lam] for lam in self.local_inequality_duals
+        ]
+        u_shape = [[] if u is None else [np.shape(u_k) for u_k in u] for u in self.local_variables]
+        p_shape = [np.shape(p) for p in self.power]
+        a_shape = [np.shape(a) for a in self.angle]
+        prices_shape = np.shape(self.prices)
+        global_angle_shape = np.shape(self.global_angle)
+
+        assert mu_shape == a_shape
+        assert prices_shape == global_angle_shape
+
+        return DispatchOutcome(
+            *[
+                mu_shape,
+                lambda_eq_shape,
+                lambda_ineq_shape,
+                u_shape,
+                p_shape,
+                a_shape,
+                prices_shape,
+                global_angle_shape,
+            ]
+        )
+
+    @cached_property
+    def blocks(self):
+        blocks, _ = self._build_blocks_recursively(self.shape, [], 0)
+        return DispatchOutcome(*blocks)
+
+    def _build_blocks_recursively(self, shape, blocks, offset):
+        # Recursive case
+        if len(shape) > 0 and isinstance(shape[0], (list, tuple)):
+            subblocks = []
+            for shape_k in shape:
+                new_block, offset = self._build_blocks_recursively(shape_k, subblocks, offset)
+                subblocks += [new_block]
+
+            return subblocks, offset
+
+        # Base case
+        if len(shape) == 0:
+            delta = 0
+        else:  # isinstance(shape[0], int)
+            print(shape)
+            delta = np.prod(shape)
+
+        return (offset, offset + delta), offset + delta
 
     def _safe_cat(self, x):
         if len(x) > 0:
@@ -40,55 +119,40 @@ class DispatchOutcome(DispatchOutcomeBase):
     def _total_len(self, variable):
         return sum([0 if x is None else sum([xi.size for xi in x]) for x in variable])
 
-    def power_indices(self):
-        return 0, self._total_len(self.power)
-
-    def angle_indices(self):
-        start = self.power_indices()[1]
-        return start, start + self._total_len(self.angle)
-
-    def local_variable_indices(self):
-        start = self.angle_indices()[1]
-        return start, start + self._total_len(self.local_variables)
-
     def torchify(self):
         # TODO
         raise NotImplementedError
 
     def vectorize(self):
-        p = self._safe_cat([np.array(p).flatten() for p in self.power])
-        a = self._safe_cat([np.array(a).flatten() for a in self.angle if a is not None])
-        u = self._safe_cat(
-            [
-                np.concatenate([ui.flatten() for ui in u])
-                for u in self.local_variables
-                if u is not None
-            ]
-        )
-        mu = self._safe_cat([np.array(mu).flatten() for mu in self.phase_duals if mu is not None])
-        lambda_eq = self._safe_cat(
-            [
-                self._safe_cat([lam.flatten() for lam in lambda_eq])
-                for lambda_eq in self.local_equality_duals
-            ]
-        )
-        lambda_ineq = self._safe_cat(
-            [
-                self._safe_cat([lam.flatten() for lam in lambda_ineq])
-                for lambda_ineq in self.local_inequality_duals
-            ]
-        )
+
+        # Duals
+        mu = self._safe_cat([np.array(mu).ravel() for mu in self.phase_duals if mu is not None])
+        lambda_eq = [
+            self._safe_cat([lam.ravel() for lam in lambda_eq])
+            for lambda_eq in self.local_equality_duals
+        ]
+        lambda_ineq = [
+            self._safe_cat([lam.ravel() for lam in lambda_ineq])
+            for lambda_ineq in self.local_inequality_duals
+        ]
+
+        # Primals
+        u = [
+            np.concatenate([ui.ravel() for ui in u]) for u in self.local_variables if u is not None
+        ]
+        p = self._safe_cat([np.array(p).ravel() for p in self.power])
+        a = self._safe_cat([np.array(a).ravel() for a in self.angle if a is not None])
 
         return self._safe_cat(
             [
-                self.global_angle.flatten(),
-                p,
-                a,
-                u,
-                self.prices.flatten(),
-                mu,
-                lambda_eq,
-                lambda_ineq,
+                mu,  # Interface
+                self._safe_cat(lambda_eq),  # Local
+                self._safe_cat(lambda_ineq),  # Local
+                self._safe_cat(u),  # Local
+                p,  # Interface
+                a,  # Interface
+                self.prices.ravel(),  # Global
+                self.global_angle.ravel(),  # Global
             ]
         )
 
@@ -194,14 +258,13 @@ class PowerNetwork:
         local_variables = nested_evaluate(local_variables)
 
         return DispatchOutcome(
-            global_angle,
-            power,
-            angle,
-            local_variables,
+            global_angle=global_angle,
+            power=power,
+            angle=angle,
+            local_variables=local_variables,
             prices=-power_balance.dual_value,
             phase_duals=[
-                [pci.dual_value for pci in pc] if pc is not None else None
-                for pc in phase_consistency
+                [pci.dual_value for pci in pc] if len(pc) > 0 else None for pc in phase_consistency
             ],
             local_equality_duals=[[lci.dual_value for lci in lc] for lc in local_equalities],
             local_inequality_duals=[[lci.dual_value for lci in lc] for lc in local_inequalities],
@@ -210,12 +273,11 @@ class PowerNetwork:
         )
 
     def kkt(self, devices, result, parameters=None):
-        if result.ground is not None:
-            devices = devices + [result.ground]
-
         if parameters is None:
             parameters = [{} for _ in devices]
-        else:
+
+        if result.ground is not None:
+            devices = devices + [result.ground]
             parameters += [{}]
 
         power = result.power
@@ -306,13 +368,59 @@ class PowerNetwork:
 
         return phase_diffs
 
-    def kkt_jacobian_variables(self, devices, dispatch_outcome, parameters=None):
-        # TODO
-        raise NotImplementedError
+    def kkt_jacobian_variables(self, devices, result, parameters=None):
+        if parameters is None:
+            parameters = [{} for _ in devices]
 
-    def kkt_jacobian_parameters(self, devices, dispatch_outcome, parameters=None):
+        if result.ground is not None:
+            devices = devices + [result.ground]
+            parameters += [{}]
+
+        # Build incidence matrix for angle-related variables
+        # For multiple time periods, we first build the incidence matrix for a single
+        # time period, then apply the Kron product with the identity matrix
+        angle_incidence = sum([d.incidence_matrix for d in devices if d.is_ac], [])
+        angle_incidence = sps.hstack(angle_incidence)
+        angle_incidence = sps.kron(sps.eye(result.time_horizon), angle_incidence)
+
+        # Part 1 - Phase duals (interface)
+        # Jacobian is just the matrices of the equality constraint: A[d][t].T @ theta - phi[d][t]
+
+        # Part 2 - Local equality duals (local)
+
+        # Part 3 - Local inequality duals (local)
+
+        # Part 4 - Local variables (local)
+
+        # Part 5 - Power (interface)
+
+        # Part 6 - Angle (interface)
+
+        # Part 7 - Prices (global)
+
+        # Part 8 - Global angle (global)
+        # Only participates in the phase consistency constraints (d = device, t = terminal)
+        # Constraint:       A[d][t].T @ theta == phi[d][t]      (dual variable mu[d][t])
+        # KKT:              A[d][t] @ mu[d][t]
+        # Jacobian:         A[d][t]                             (in column of mu[d][t])
+
+        return angle_incidence
+
+    def kkt_jacobian_parameters(self, devices, result, parameters=None):
         # TODO
         raise NotImplementedError
 
     # def vector_jacobian_product(self, devices, dispatch_outcome, y):
     #     pass
+
+
+# [
+#     mu,  # Interface
+#     self._safe_cat(lambda_eq),  # Local
+#     self._safe_cat(lambda_ineq),  # Local
+#     self._safe_cat(u),  # Local
+#     p,  # Interface
+#     a,  # Interface
+#     self.prices.flatten(),  # Global
+#     self.global_angle.flatten(),  # Global
+# ]
