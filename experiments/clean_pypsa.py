fe88e8f388c43192e65884ab67bc75ea8a16b8b0
Anthony Degleris <degleris1@gmail.com>
2024-04-21 16:33:10 -0700

    one more try at fixing paths

--- experiments/clean_pypsa.py
+++ experiments/clean_pypsa.py
@@ -1,13 +1,14 @@
-import os
 import pypsa
+from pathlib import Path
 
-FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+ZAP_PATH = Path(__file__).resolve().parent.parent
 NUM_NODES = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 
 for num_nodes in NUM_NODES:
     for ext in ["", "_ec"]:
         pn = pypsa.Network(f"~/pypsa-usa/workflow/resources/western/elec_s_{num_nodes}{ext}.nc")
-        csv_folder_path = os.path.join(
-            FILE_PATH, "..", "data", "pypsa", "western", f"elec_s_{num_nodes}{ext}"
-        )
+
+        csv_folder_path = ZAP_PATH / "data" / "pypsa" / "western" / f"elec_s_{num_nodes}{ext}"
+        csv_folder_path.mkdir(parents=True, exist_ok=True)
+
         pn.export_to_csv_folder(csv_folder_path)
