1a6183b5a4de2746209f3ebc3d84b741fc83f352
Anthony Degleris <degleris1@gmail.com>
2024-04-21 16:27:54 -0700

    fix export filepath

--- experiments/clean_pypsa.py
+++ experiments/clean_pypsa.py
@@ -1,8 +1,14 @@
+import os
 import pypsa
+
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 NUM_NODES = range(100, stop=1100, step=100)
 
 for num_nodes in NUM_NODES:
     for ext in ["", "_ec"]:
         pn = pypsa.Network(f"~/pypsa-usa/workflow/resources/western/elec_s_{num_nodes}{ext}.nc")
-        pn.export_to_csv_folder(f"../data/pypsa/western/elec_s_{num_nodes}{ext}/")
+        csv_folder_path = os.path.join(
+            FILE_PATH, "..", "data", "pypsa", "western", f"elec_s_{num_nodes}{ext}"
+        )
+        pn.export_to_csv_folder(csv_folder_path)
