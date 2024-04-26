d8aaeece894de8051fa68c4dbfeb2fab7f1d43c2
Anthony Degleris <degleris1@gmail.com>
2024-04-26 12:37:17 -0700

    parallel benchmark launch

--- experiments/launch.py
+++ experiments/launch.py
@@ -4,19 +4,24 @@ import runner
 
 
 config_path = sys.argv[1]
-config = runner.load_config(config_path)
-system = config["system"]
+config_list = runner.expand_config(runner.load_config(config_path))
 
-# Configure file paths
-output_file = runner.datadir("slurm", f"{config['name']}.out")
-script_file = runner.datadir("scripts", f"{config['name']}.sh")
+print(f"Launching {len(config_list)} jobs...")
+for i, config in enumerate(config_list):
+    assert i == config["index"]
 
-output_file.parent.mkdir(parents=True, exist_ok=True)
-script_file.parent.mkdir(parents=True, exist_ok=True)
+    system = config["system"]
 
-# Write slurm script
-slurm_script = f"""#!/bin/bash
-#SBATCH --job-name={config["name"]}
+    # Configure file paths
+    output_file = runner.datadir("slurm", f"{config['name']}_{i:03d}.out")
+    script_file = runner.datadir("scripts", f"{config['name']}_{i:03d}.sh")
+
+    output_file.parent.mkdir(parents=True, exist_ok=True)
+    script_file.parent.mkdir(parents=True, exist_ok=True)
+
+    # Write slurm script
+    slurm_script = f"""#!/bin/bash
+#SBATCH --job-name={config["name"]}_{i:03d}
 #SBATCH --output={output_file}
 #SBATCH --qos=shared
 #SBATCH --constraint=cpu
@@ -27,12 +32,12 @@ slurm_script = f"""#!/bin/bash
 
 module load conda
 conda activate $ZAP_ENV
-srun python -u experiments/runner.py {config_path}
+srun python -u experiments/runner.py {config_path} {i}
 """
 
-with open(script_file, "w") as f:
-    f.write(slurm_script)
+    with open(script_file, "w") as f:
+        f.write(slurm_script)
 
-# Launch
-print(f"Launching job {config['name']}")
-os.system(f"sbatch {script_file}")
+    # Launch
+    print(f"Launching job {config['name']} (parameter {i})...")
+    os.system(f"sbatch {script_file}")
